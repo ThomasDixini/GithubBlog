6c555512743be37e4f8ed7593838f3ec5d2afdb2
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-11 20:10:52 -0300

    Repositorie(feat): estrutura HTML da segunda página criada

--- src/pages/Repositorie/Repositorie.tsx
+++ src/pages/Repositorie/Repositorie.tsx
@@ -2,5 +2,33 @@ import { useParams } from 'react-router-dom'
 
 export function Repositorie(){
     const { repo } = useParams()
-    return <h1>Repositorie id:{repo} </h1>
+    return (
+        <main>
+            <section>
+                <header>
+                    <a href="#"> Voltar </a>
+                    <a href="#"> VER NO GITHUB </a>
+                </header>
+                <strong> JavaScript data types and data structures </strong>
+                <footer>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </footer>
+            </section>
+            <section>
+                <dl>
+                    <dt> Dynamic Js </dt>
+                    <dd> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quo officiis rerum aliquid iste ex dolores veritatis a
+                        lias quod nihil repellat quaerat quia, minus consectetur! 
+                        Numquam saepe quaerat ipsum quam modi!
+                    </dd>
+                </dl>
+                <pre>
+                       asfas
+                </pre>
+            </section>  
+        </main>
+    );
 }
