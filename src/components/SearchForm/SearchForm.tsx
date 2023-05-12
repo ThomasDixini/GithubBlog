545491d8cf32a201fe4c320cce8386d060bad0ae
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-12 20:01:36 -0300

    Hookform(feat): utilizando da lib react-hook-form para tipar e validar os inputs

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,13 +1,38 @@
+import { useForm } from "react-hook-form";
 import { FormContainer } from "./styles";
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+const SearchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchIssueType = z.infer<typeof SearchFormSchema>
 
 export function SearchForm(){
+    
+    const { register, handleSubmit } = useForm<SearchIssueType>({
+        resolver: zodResolver(SearchFormSchema),
+        defaultValues: {
+            query: ''
+        }
+    });
+
+    function handleSearchIssues(){
+
+    }
+
     return(
-        <FormContainer action="">
+        <FormContainer action="" onSubmit={handleSubmit(handleSearchIssues)}>
             <div>
                 <label htmlFor="searchForm"> Publicações </label>
                 <span> 6 publicações </span>
             </div>
-            <input type="text" placeholder="Buscar conteúdo"/>
+            <input 
+                type="text" 
+                placeholder="Buscar conteúdo"
+                {...register("query")}
+            />
         </FormContainer>
     );
 }
